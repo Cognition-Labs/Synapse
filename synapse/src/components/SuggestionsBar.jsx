7846979fb591662182a1cd74f0d4c791f54e4be7
Daniel George <degtrdg@github.com>
2023-07-02 14:23:21 -0500

    update empty curr paragraph

--- synapse/src/components/SuggestionsBar.jsx
+++ synapse/src/components/SuggestionsBar.jsx
@@ -1,66 +1,84 @@
 import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { zoteroQueryAtom } from "../atoms";
+import { Box, Button, Collapse, VStack, Text, Divider } from "@chakra-ui/react";
 
 function SuggestionsBar() {
   const zoteroQuery = useRecoilValue(zoteroQueryAtom);
   const [documents, setDocuments] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [showSource, setShowSource] = useState({});
 
   useEffect(() => {
     if (isFetching) return;
-
     async function fetchModifiedQuery() {
+      if (!zoteroQuery || zoteroQuery.trim() === "") {
+        setIsFetching(false);
+        return;
+      }
       setIsFetching(true);
-      console.log("Fetching modified query");
-
       const response = await fetch(
         `https://degtrdg--synapse-run-query.modal.run/?query=` +
           zoteroQuery +
           "&db_name=daniel",
-        {
-          method: "GET",
-        }
+        { method: "GET" }
       );
-
-      console.log("Received response from server", response);
       if (!response.ok) {
-        console.error("Failed to fetch modified query", response);
         setIsFetching(false);
         return;
       }
-
       const text = await response.text();
-      console.log("text", text);
       let parsed;
       try {
         parsed = JSON.parse(text);
       } catch (error) {
-        console.error(`Invalid JSON: ${error.message}`);
         setIsFetching(false);
         return;
       }
       setDocuments(parsed);
       setIsFetching(false);
     }
-    const fetchTimeout = setTimeout(fetchModifiedQuery, 3000); // Waits 3 second before fetching
-    return () => clearTimeout(fetchTimeout); // Clears the timeout if the component unmounts
+    const fetchTimeout = setTimeout(fetchModifiedQuery, 3000);
+    return () => clearTimeout(fetchTimeout);
   }, [zoteroQuery, isFetching]);
 
+  const handleToggle = (index) => {
+    setShowSource((prev) => ({ ...prev, [index]: !prev[index] }));
+  };
+
   return (
-    <div>
+    <VStack
+      align="start"
+      overflowY="scroll"
+      // width="400px"
+      height="100vh"
+      p={5}
+      spacing={5}
+      border="1px"
+      borderColor="gray.200"
+    >
       {documents.length > 0 ? (
         documents.map((doc, index) => (
-          <div key={index}>
-            <div>{doc.page_content}</div>
-            <div>Source: {doc.metadata.source}</div>
-            <div>Page: {doc.metadata.page}</div>
-          </div>
+          <Box key={index} w="100%">
+            <Text>{doc.page_content}</Text>
+            <Button size="sm" onClick={() => handleToggle(index)} mt={2}>
+              {showSource[index] ? "Hide Source" : "Show Source"}
+            </Button>
+            <Collapse in={showSource[index]}>
+              <Text fontSize="sm" color="gray.600" mt={2}>
+                {doc.metadata.source}
+              </Text>
+            </Collapse>
+            <Text fontSize="sm" color="gray.600" mt={2}>
+              Page: {doc.metadata.page}
+            </Text>
+            <Divider mt={4} />
+          </Box>
         ))
       ) : (
-        <div>Loading...</div>
+        <Box>Loading...</Box>
       )}
-    </div>
+    </VStack>
   );
 }
 
